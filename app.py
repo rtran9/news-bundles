322931fe57cc95165fb9ee06c36493f905fe96ec
Jasmin Rubinovitz <jasrub@gmail.com>
2016-09-14 23:33:42 -0400

    test schedualer

--- app.py
+++ app.py
@@ -5,18 +5,28 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
+def my_listener(event):
+    if event.exception:
+        print('The job crashed :(')
+    else:
+        print('The job worked :)')
+
+
 @app.before_first_request
 def initialize():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(cach_data, 'interval', hours=5)
+    scheduler.add_job(cach_data, 'interval', minutes=5)
+    scheduler.add_listener(my_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
     scheduler.start()
 
 
 def cach_data():
+	print ("starting cach data")
 	data = get_data()
 	with open('static/data/data.json', 'w') as outfile:
 		json.dump(data, outfile)
 		print ('data saved to file')
+	print ("cach data finished")
 
 @app.route("/")
 def index():
